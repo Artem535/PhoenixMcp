ac547b8b37fe184139c6731556480601d6bee424
Artem Durynin <artem.durynin@raftds.com>
2025-11-16 17:01:34 +0300

    feat: Add support custom output

--- examples/create_server/main.cpp
+++ examples/create_server/main.cpp
@@ -9,21 +9,32 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-struct MyToolInput {
+struct BasicToolInput {
   int a;
   int b;
 };
 
-auto sum_two_numbers(const MyToolInput& input) {
+struct BasicToolOutput {
+  int mul_result;
+};
+
+auto sum_two_numbers(const BasicToolInput& input) {
   rfl::Generic::Object obj;
   obj["sum"] = input.a + input.b;
   return pxm::utils::make_text_result(rfl::json::write(obj));
 }
 
+BasicToolOutput mul_two_numbers(const BasicToolInput& input) {
+  return {.mul_result = input.a * input.b};
+}
+
 int main() {
   auto registry = std::make_unique<pxm::tool::ToolRegistry>();
-  registry->register_tool<MyToolInput>("sum_tool", "Sum two int numbers",
-                                       sum_two_numbers);
+  registry->register_tool<BasicToolInput>("sum_tool", "Sum two int numbers",
+                                          sum_two_numbers);
+
+  registry->register_tool<BasicToolInput, BasicToolOutput>(
+      "mul_tool", "Mul two int numbers", mul_two_numbers);
 
   auto transport = std::make_unique<pxm::server::StdioTransport>();
 
@@ -37,16 +48,16 @@ int main() {
 
   // Setup spdlog to log to a file
   auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-    "./mcp_server.log", true
-  );
+      "./mcp_server.log", true
+      );
 
   spdlog::set_default_logger(std::make_shared<spdlog::logger>(
-    "multi", spdlog::sinks_init_list{file_sink}
-  ));
+      "multi", spdlog::sinks_init_list{file_sink}
+      ));
 
   spdlog::set_level(spdlog::level::debug);
   spdlog::flush_on(spdlog::level::debug);
 
-  spdlog::info("Test:{}", rfl::json::to_schema<MyToolInput>());
+  spdlog::info("Test:{}", rfl::json::to_schema<BasicToolInput>());
   return server.start_server();
 }
--- src/phoenix_mcp/tool_registry/tool_registry.h
+++ src/phoenix_mcp/tool_registry/tool_registry.h
@@ -11,6 +11,7 @@
 #include <rfl/json.hpp>
 #include <rfl/Generic.hpp>
 
+#include "utils.hpp"
 #include "../types/msg_types.hpp"
 
 namespace pxm::tool {
@@ -20,9 +21,12 @@ using ToolHandlerInternal = std::function<msg::types::CallToolResult(
 
 /// @brief Template function type for tool handlers with specific parameter types
 /// @tparam InputParams The parameter struct type for this tool
-template <typename Params>
+template <typename InputParams>
 using ToolHandler = std::function<msg::types::CallToolResult(
-    const Params& params)>;
+    const InputParams& params)>;
+
+template <typename InputParams, typename OutputParams>
+using ToolHandlerWithOutput = std::function<OutputParams(const InputParams& params)>;
 
 /// @brief Registry for managing available tools in the MCP server
 /// 
@@ -67,6 +71,44 @@ public:
       InputParams params = rfl::from_generic<InputParams>(generic_params).value();
       // Call the actual handler
       return handler(params);
+    };
+
+    spdlog::debug("ToolRegistry::register_tool| Tool {} registered", name);
+  }
+
+  template <typename InputParams, typename OutputParams>
+  void register_tool(const std::string& name, const std::string& description,
+                     const ToolHandlerWithOutput<InputParams, OutputParams>& handler) {
+    // Generate JSON schema for the parameter type
+    const auto schema_str = rfl::json::to_schema<InputParams>();
+    auto schema = rfl::json::read<msg::types::ToolInputSchema>(schema_str).
+        value();
+
+    std::string ref = schema.ref.value();
+    const size_t suffix_position = ref.find_last_of('/');
+    ref = ref.substr(suffix_position + 1);
+
+    // Create tool description structure
+    const msg::types::Tool tool = {
+      .name = name,
+      .description = description,
+      .input_schema = schema.defs.value()[ref],
+  };
+
+    // Log the tool specification for debugging
+    spdlog::debug(
+        "ToolRegistry::register_tool| Create the tool with this specification: {}",
+        rfl::json::write(tool));
+
+    // Store tool description and wrap handler for internal use
+    tool_descriptions_[name] = tool;
+    tools_[name] = [handler](const rfl::Generic& generic_params) {
+      // Convert generic parameters to the specific type
+      InputParams params = rfl::from_generic<InputParams>(generic_params).value();
+      // Call the actual handler
+      const auto output = handler(params);
+      const auto output_str = rfl::json::write(output);
+      return utils::make_text_result(output_str);
     };
 
     spdlog::debug("ToolRegistry::register_tool| Tool {} registered", name);
